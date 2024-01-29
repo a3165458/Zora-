18ad5fefa55b17c920e4c7132f5e37c040e0ae4f
a3165458 <48400087+a3165458@users.noreply.github.com>
2024-01-29 10:37:35 +0800

    Update Zora2.sh

--- Zora2.sh
+++ Zora2.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# 安装Docker
-sudo apt-get install docker.io
-sudo systemctl start docker
-
-
 # 运行节点
 echo "启动 Docker 容器..."
-docker compose up --build
+docker-compose up --build
 
 # 设置自动重启
 echo "设置容器自动重启..."
