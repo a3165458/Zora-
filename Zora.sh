5cf5f4e3d9c133bf5d2dba5dab05115e5ba9bac0
a3165458 <48400087+a3165458@users.noreply.github.com>
2024-01-29 10:56:19 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -19,6 +19,9 @@ cp .env.example .env
 # Note: This will open the nano editor, the script will pause until you exit nano
 nano .env
 
+# 启动Screen
+screen -S Zora
+
 # 运行节点
 echo "启动 Docker 容器..."
 docker-compose up --build
@@ -27,9 +30,3 @@ docker-compose up --build
 echo "设置容器自动重启..."
 docker update --restart=unless-stopped node-node-1
 docker update --restart=unless-stopped node-geth-1
-
-# 检查日志
-echo "正在查看 node-node-1 的日志..."
-docker logs -f node-node-1 &
-echo "正在查看 node-geth-1 的日志..."
-docker logs -f node-geth-1 &
