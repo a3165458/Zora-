87a0e7ff6a0a58160c105ba9c019644355aca22b
a3165458 <48400087+a3165458@users.noreply.github.com>
2024-01-29 10:39:51 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-# Stop the script if any command fails
+
 set -e
 
-# 1. Update and install packages
+# 更新组件
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
+sudo apt-get install docker-compose
 
-# 2. Download Source
+# 下载资源
 git clone https://github.com/conduitxyz/node.git
 cd node
 ./download-config.py zora-mainnet-0
 export CONDUIT_NETWORK=zora-mainnet-0
 cp .env.example .env
 
-# 3. Edit .env
+# 修改env
 # Note: This will open the nano editor, the script will pause until you exit nano
 nano .env
+
+# 运行节点
+echo "启动 Docker 容器..."
+docker-compose up --build
+
+# 设置自动重启
+echo "设置容器自动重启..."
+docker update --restart=unless-stopped node-node-1
+docker update --restart=unless-stopped node-geth-1
+
+# 检查日志
+echo "正在查看 node-node-1 的日志..."
+docker logs -f node-node-1 &
+echo "正在查看 node-geth-1 的日志..."
+docker logs -f node-geth-1 &
