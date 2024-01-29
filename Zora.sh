7136b2c7d098ca06df6aa824ba16b5e0c257ded2
a3165458 <48400087+a3165458@users.noreply.github.com>
2024-01-29 11:44:48 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -18,15 +18,3 @@ cp .env.example .env
 # 修改env
 # Note: This will open the nano editor, the script will pause until you exit nano
 nano .env
-
-# 启动Screen
-screen -S Zora
-
-# 运行节点
-echo "启动 Docker 容器..."
-docker-compose up --build
-
-# 设置自动重启
-echo "设置容器自动重启..."
-docker update --restart=unless-stopped node-node-1
-docker update --restart=unless-stopped node-geth-1
