fe2c2c96b3af4fabd97022b6bd31ef614a1f887b
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-02 16:58:16 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-
-set -e
-
 # 更新组件
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates nano gnupg lsb-release -y
-sudo apt-get install docker-compose
+
+# 安装Docker
+# Check if Docker is installed
+if command -v docker &> /dev/null; then
+    echo "Docker is already installed. Skipping installation."
+else
+    # Install Docker
+    sudo apt-get install ca-certificates curl gnupg -y
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
+    systemctl start docker
+    systemctl enable docker
+fi
+
+# Check if Docker Compose is installed
+if command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is already installed. Skipping installation."
+else
+    # Install Docker Compose
+    sudo apt-get install docker-compose -y
+fi
+
 
 # 下载资源
 git clone https://github.com/conduitxyz/node.git
@@ -15,6 +42,10 @@ cd node
 export CONDUIT_NETWORK=zora-mainnet-0
 cp .env.example .env
 
+
 # 修改env
 # Note: This will open the nano editor, the script will pause until you exit nano
 nano .env
+
+# Docker组建
+docker-compose up --build
