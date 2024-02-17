458b9ccb58a6e98fbea104152cdd2f51c5d38cf2
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-17 13:02:33 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -4,35 +4,47 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates nano gnupg lsb-release -y
 
-# 安装Docker
-# Check if Docker is installed
-if command -v docker &> /dev/null; then
-    echo "Docker is already installed. Skipping installation."
-else
-    # Install Docker
-    sudo apt-get install ca-certificates curl gnupg -y
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# 升级所有已安装的包
+sudo apt upgrade -y
 
+
+# 检查 Docker 是否已安装
+if ! command -v docker &> /dev/null
+then
+    # 如果 Docker 未安装，则进行安装
+    echo "未检测到 Docker，正在安装..."
+    sudo apt-get install ca-certificates curl gnupg lsb-release
+
+    # 添加 Docker 官方 GPG 密钥
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    # 设置 Docker 仓库
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-    sudo apt-get update -y
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
-    systemctl start docker
-    systemctl enable docker
-fi
+    # 授权 Docker 文件
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo apt-get update
 
-# Check if Docker Compose is installed
-if command -v docker-compose &> /dev/null; then
-    echo "Docker Compose is already installed. Skipping installation."
+    # 安装 Docker 最新版本
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 else
-    # Install Docker Compose
-    sudo apt-get install docker-compose -y
+    echo "Docker 已安装。"
 fi
+
+# 检查 Docker Compose 是否已安装
+if ! command -v docker-compose &> /dev/null
+then
+    echo "未检测到 Docker Compose，正在安装..."
+    sudo apt install docker-compose -y
+else
+    echo "Docker Compose 已安装。"
+fi
+
+# 验证 Docker Engine 安装是否成功
+sudo docker run hello-world
 
 
 # 下载资源
