254010500cecbe2830133199c5d0cb598b74fd27
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-18 10:15:18 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -47,7 +47,7 @@ else
         # 移除旧版本
         sudo rm $(which docker-compose)
         # 安装新版本
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
     else
         echo "Docker Compose 的当前版本为 $CURRENT_VER，满足最小要求版本 ($MIN_VER)。"
