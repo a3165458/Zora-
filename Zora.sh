c96056e84d2a1f315e09b3739247e2d7d6bf61f1
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-18 10:34:40 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -7,55 +7,45 @@ sudo apt install curl build-essential git screen jq pkg-config libssl-dev libcla
 # 升级所有已安装的包
 sudo apt upgrade -y
 
+
 # 检查 Docker 是否已安装
 if ! command -v docker &> /dev/null
 then
+    # 如果 Docker 未安装，则进行安装
     echo "未检测到 Docker，正在安装..."
-    # 安装 Docker 前的准备
-    sudo apt-get install ca-certificates curl gnupg lsb-release -y
+    sudo apt-get install ca-certificates curl gnupg lsb-release
+
     # 添加 Docker 官方 GPG 密钥
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
     # 设置 Docker 仓库
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
     # 授权 Docker 文件
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
     sudo apt-get update
+
     # 安装 Docker 最新版本
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 else
     echo "Docker 已安装。"
 fi
 
-# 定义版本比较函数
-version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
-
-# 检查 Docker Compose 是否已安装并检查其版本
+# 检查 Docker Compose 是否已安装
 if ! command -v docker-compose &> /dev/null
 then
     echo "未检测到 Docker Compose，正在安装..."
     sudo apt install docker-compose -y
 else
-    # 获取当前 Docker Compose 的版本
-    CURRENT_VER=$(docker-compose --version | sed 's/docker-compose version \(.*\),.*/\1/')
-    MIN_VER="1.29.2"
-    # 比较当前版本与最小要求版本
-    if version_gt "$MIN_VER" "$CURRENT_VER"; then
-        echo "Docker Compose 的当前版本 ($CURRENT_VER) 低于最小要求版本 ($MIN_VER)，正在升级..."
-        # 移除旧版本
-        sudo rm $(which docker-compose)
-        # 安装新版本
-        sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-    else
-        echo "Docker Compose 的当前版本为 $CURRENT_VER，满足最小要求版本 ($MIN_VER)。"
-    fi
+    echo "Docker Compose 已安装。"
 fi
 
 # 验证 Docker Engine 安装是否成功
 sudo docker run hello-world
+
 
 # 下载资源
 git clone https://github.com/conduitxyz/node.git
@@ -71,4 +61,4 @@ read -p "请输入对应L1ETH的RPC网址: " OP_NODE_L1_ETH_RPC
 sed -i "s|OP_NODE_L1_ETH_RPC=.*|OP_NODE_L1_ETH_RPC=${OP_NODE_L1_ETH_RPC}|" .env
 
 # Docker组建
-docker-compose up --build -d
+docker compose up --build -d
