09c5dd2bec2dcc04242bc51ffa5b7a6e5ac01555
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-03 12:33:17 +0800

    Update Zora.sh

--- Zora.sh
+++ Zora.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+# 脚本保存路径
+SCRIPT_PATH="$HOME/Zora.sh"
+
+# 自动设置快捷键的功能
+function check_and_set_alias() {
+    local alias_name="zora"
+    local shell_rc="$HOME/.bashrc"
+
+    # 对于Zsh用户，使用.zshrc
+    if [ -n "$ZSH_VERSION" ]; then
+        shell_rc="$HOME/.zshrc"
+    elif [ -n "$BASH_VERSION" ]; then
+        shell_rc="$HOME/.bashrc"
+    fi
+
+    # 检查快捷键是否已经设置
+    if ! grep -q "$alias_name" "$shell_rc"; then
+        echo "设置快捷键 '$alias_name' 到 $shell_rc"
+        echo "alias $alias_name='bash $SCRIPT_PATH'" >> "$shell_rc"
+        # 添加提醒用户激活快捷键的信息
+        echo "快捷键 '$alias_name' 已设置。请运行 'source $shell_rc' 来激活快捷键，或重新打开终端。"
+    else
+        # 如果快捷键已经设置，提供一个提示信息
+        echo "快捷键 '$alias_name' 已经设置在 $shell_rc。"
+        echo "如果快捷键不起作用，请尝试运行 'source $shell_rc' 或重新打开终端。"
+    fi
+}
+
+# 节点安装功能
+function install_node() {
+
 # 更新组件
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates nano gnupg lsb-release -y
@@ -62,3 +93,36 @@ sed -i "s|OP_NODE_L1_ETH_RPC=.*|OP_NODE_L1_ETH_RPC=${OP_NODE_L1_ETH_RPC}|" .env
 
 # Docker组建
 docker compose up --build -d
+
+}
+
+# 查看节点日志
+function check_service_status() {
+    cd node
+    docker compose logs -f
+}
+
+
+# 主菜单
+function main_menu() {
+    clear
+    echo "脚本以及教程由推特用户大赌哥 @y95277777 编写，免费开源，请勿相信收费"
+    echo "================================================================"
+    echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
+    echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
+    echo "请选择要执行的操作:"
+    echo "1. 安装节点"
+    echo "2. 查看节点日志"
+    echo "3. 设置快捷键的功能"
+    read -p "请输入选项（1-2）: " OPTION
+
+    case $OPTION in
+    1) install_node ;;
+    2) check_service_status ;;
+    3) check_and_set_alias ;;  
+    *) echo "无效选项。" ;;
+    esac
+}
+
+# 显示主菜单
+main_menu
